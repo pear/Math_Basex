256213e406c691ce5e6ca50faff9a42b0b8ad2cc
Dave Mertens <zyprexia@php.net>
2002-12-10 17:48:43 +0000

    - Changed backslash to slash - Added results to test calls
    git-svn-id: http://svn.php.net/repository/pear/packages/Math_Basex/trunk@107449 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Basex.php
+++ tests/Basex.php
@@ -1,13 +1,14 @@
 <?php
+
 //include BaseX class
-include_once( "Math\Basex.php" );
+include_once( "Math/Basex.php" );
 
 //Create new instance
 $base6 = new basex("ABCDEF");
 
 //just calling some methods for testing Basex class.
-echo "basex::encode( 123456 )  --> " . $base6->encode( 123456 ) . "\n";
-echo "basex::decode( \"BADA\" ) --> " . $base6->decode( "BADA" ) . "\n";
+echo "basex::encode( 123456 )  --> " . $base6->encode( 123456 ) . " (Result: CDFBDCA)\n";
+echo "basex::decode( \"BADA\" ) --> " . $base6->decode( "BADA" ) . " (Result: 234)\n";
 
 
 ?>
