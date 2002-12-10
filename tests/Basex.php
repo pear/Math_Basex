8e1ef40d1d30f2dffc5e6ad6f753c7709b020a8b
Dave Mertens <zyprexia@php.net>
2002-12-10 14:18:48 +0000

    Added packge information (had some free minutes) and another test. Maybe tonight I can add the results for the tests..
    git-svn-id: http://svn.php.net/repository/pear/packages/Math_Basex/trunk@107390 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/Basex.php
@@ -0,0 +1,13 @@
+<?php
+//include BaseX class
+include_once( "Math\Basex.php" );
+
+//Create new instance
+$base6 = new basex("ABCDEF");
+
+//just calling some methods for testing Basex class.
+echo "basex::encode( 123456 )  --> " . $base6->encode( 123456 ) . "\n";
+echo "basex::decode( \"BADA\" ) --> " . $base6->decode( "BADA" ) . "\n";
+
+
+?>
